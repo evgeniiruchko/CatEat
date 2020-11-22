40813ed743894a0c4323643bc5f7a4e75fcbfee5
EvgeniiRuchko <gk1987@mail.ru>
2020-11-22 14:56:13 +0300

    Домашнее задание к 8 уроку

--- src/ru/geekbrains/CatEat/Main.java
+++ src/ru/geekbrains/CatEat/Main.java
@@ -1,29 +1,36 @@
 package ru.geekbrains.CatEat;
 
-import java.lang.reflect.Array;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Main {
+public class Main extends JFrame {
 
     public static void main(String[] args) {
         Cat catTom = new Cat("Том", 10);
         Cat catFelix = new Cat("Феликс", 5);
         Cat catGav = new Cat("Гав", 1);
         Plate Plate = new Plate(10);
-        Cat[] catArray = {catFelix,catTom,catGav};
+        Cat[] catArray = {catFelix, catTom, catGav};
 
         Plate.info();
 
-        for (int i = 0; i < catArray.length; i++)
-        {
-            catArray[i].eat(Plate);
-            Plate.info();
-        }
-        Plate.addEat(6);
-        for (int i = 0; i < catArray.length; i++)
-        {
-            catArray[i].eat(Plate);
-            Plate.info();
-        }
+        new MyWindow(Plate, catArray[0], catArray[1], catArray[2]);
+//
+//        for (int i = 0; i < catArray.length; i++)
+//        {
+//            catArray[i].eat(Plate);
+//            Plate.info();
+//        }
+//        Plate.addEat(6);
+//        for (int i = 0; i < catArray.length; i++)
+//        {
+//            catArray[i].eat(Plate);
+//            Plate.info();
+//        }
 
     }
 }
+
+
